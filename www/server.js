632e0c24a2f3b990c2a0262ecc472cc2aaa2f9fc
ybzjsxh <musiq841@gmail.com>
2020-01-07 18:36:11 +0800

    v.1.1.1 add logger

--- www/server.js
+++ www/server.js
@@ -5,14 +5,18 @@ const multer = require('multer');
 const bodyParser = require('body-parser');
 const chalk = require('chalk');
 const fs = require('fs');
+const os = require('os');
+const { net, err } = require('./logger');
 const path = require('path');
 const { spawn, spawnSync } = require('child_process');
 
 const port = process.env.PORT || 3001;
 
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+let platform = os.platform();
 
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+
+app.use(bodyParser.json({ limit: '10mb' }));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
@@ -22,21 +26,23 @@ router.post('/update', (req, res, next) => {
     path.join(__dirname, '../resources/app.asar.unpacked/config.json'),
     'utf8',
   );
-  let new_config = {}
+  let new_config = {};
   if (!!content) {
     new_config = { ...JSON.parse(config), content, fontSize, fontColor: color };
   } else {
-    new_config = { ...JSON.parse(config), fontSize, fontColor: color};
+    new_config = { ...JSON.parse(config), fontSize, fontColor: color };
   }
   fs.writeFile(
     path.join(__dirname, '../resources/app.asar.unpacked/config.json'),
     JSON.stringify(new_config),
-    err => {
-      if (!err) {
+    error => {
+      if (!error) {
         console.log('update success');
+        net.info(`update config: ${content} ${fontSize} ${color}`);
         res.json({ code: 0, msg: 'ok', data: { content, fontSize, color } });
       } else {
-        res.json({ code: -1, msg: err.msg });
+        err.error(`update error: ${error}`);
+        res.json({ code: -1, msg: error.msg });
       }
     },
   );
@@ -63,23 +69,49 @@ router.post(
       'upload_tmp/06.png',
       path.join(__dirname, '../resources/app.asar.unpacked/static/images/'),
     ]);
+    net.info(`上传文件成功：${file.filename}`);
     res.json({ code: 0, data: { file: file.filename, msg: '上传成功!' } });
   },
 );
 
 router.post('/restart', (req, res) => {
   try {
-    fs.readFile(path.join(__dirname, './pid.txt'), 'utf8', (err, pid) => {
-      console.log('current pid: ', pid);
-      spawnSync('taskkill', ['/pid', `${pid}`, '-t', '-f']);
+    fs.readFile(path.join(__dirname, './pid.txt'), 'utf8', (error, pid) => {
+      if (error) {
+        console.log(error);
+        err.error(`读取pid失败：${error}`);
+      } else {
+        console.log('current pid: ', pid);
+        if (platform === 'win32') {
+          net.info(`执行命令：taskkill /pid ${pid} -t -f`);
+          spawnSync('taskkill', ['/pid', `${pid}`, '-t', '-f']);
+
+          console.log('restarting app');
+          net.info(`restarting app`);
+          net.info(`启动程序---windows`);
+          spawn(path.join(__dirname, '../welcomeconfig.exe'), {
+            cwd: path.join(__dirname, '../'),
+          });
+          net.info(`重启成功！`);
+          res.json({ code: 0, msg: 'ok' });
+        } else {
+          net.info(`执行命令：sudo kill -9 ${pid}`);
+          spawnSync('sudo', ['kill', '-9', `${pid}`]);
+
+          console.log('restarting app');
+          net.info(`restarting app`);
+          net.info(`启动程序---linux`);
+          spawn(path.join(__dirname, '../welcomeconfig'), {
+            cwd: path.join(__dirname, '../'),
+          });
+          net.info(`重启成功！`);
+          res.json({ code: 0, msg: 'ok' });
+        }
+      }
     });
-    console.log('restarting app');
-    spawn(path.join(__dirname, '../welcomeconfig.exe'), {
-      cwd: path.join(__dirname, '../'),
-    });
-    res.json({ code: 0, msg: 'ok' });
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error);
+    err.error(`重启失败：${error}`);
     res.json({ code: -1, msg: 'error' });
   }
 });
@@ -87,5 +119,6 @@ router.post('/restart', (req, res) => {
 app.use('/api', router);
 
 app.listen(port, () => {
+  net.info(`后台服务启动成功：http://127.0.0.1:${port}`);
   console.log(chalk.blue(`listening on http://127.0.0.1:${port}`));
 });
